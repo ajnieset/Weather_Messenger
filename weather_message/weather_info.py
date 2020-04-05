d35c54bf8ef575c72f1f19441afaa6308c519109
Austin Nieset <ajnieset@gmail.com>
2020-04-05 15:30:56 -0400

    added code to make api call

--- weather_message/weather_info.py
+++ weather_message/weather_info.py
@@ -1,7 +1,18 @@
+from typing import Final
+
 import requests
+import toml
 
-URL = ""
+
+with open("configs/config.toml", "r") as cfg:
+    CONFIG = toml.load(cfg)
+
+DARKSKY: Final = "https://api.darksky.net/forecast/"
 
 
-def get_weather_week():
-    return requests.get(URL).json()
+def get_daily_weather(latitude: str, longitude: str) -> dict:
+    """ Returns the daily weather information for a week """
+
+    return requests.get(
+        DARKSKY + CONFIG["dark-sky"]["appid"] + f"/{latitude}, {longitude}"
+    ).json()
