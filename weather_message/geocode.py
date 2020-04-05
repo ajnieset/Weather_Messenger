b5ec51423010d7292c224ddf7e5f86ee5126f4ad
Austin Nieset <ajnieset@gmail.com>
2020-04-05 18:42:29 -0400

    Implemented geocode library

--- weather_message/geocode.py
+++ weather_message/geocode.py
@@ -1,12 +1,11 @@
 import toml
+from geocodio import GeocodioClient  # type: ignore
 
-from typing import Final
 
 with open("configs/config.toml", "r") as cfg:
     CONFIG = toml.load(cfg)
 
-GEOCODIO: Final = "https://api.geocod.io/v1.4/geocode"
 
-
-def get_coordinates(location):
-    pass
+def get_coordinates(location: str) -> tuple:
+    client = GeocodioClient(CONFIG["geocod"]["key"])
+    return client.geocode(location).coords
