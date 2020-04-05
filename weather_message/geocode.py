e57dda651ea89e2546cdc4b7635759ae4f9b9b39
Austin Nieset <ajnieset@gmail.com>
2020-04-05 15:38:13 -0400

    added url for api call

--- weather_message/geocode.py
+++ weather_message/geocode.py
@@ -1,7 +1,11 @@
 import toml
 
+from typing import Final
+
 with open("configs/config.toml", "r") as cfg:
     CONFIG = toml.load(cfg)
+
+GEOCODIO: Final = "https://api.geocod.io/v1.4/geocode"
 
 
 def get_coordinates(location):
